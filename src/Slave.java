faa38dcd09b5a5dc94bef1d679094c32c1cbe132
Gastordia <147313760+Gastordia@users.noreply.github.com>
2024-01-16 13:46:52 +0100

    Update Slave.java

--- src/Slave.java
+++ src/Slave.java
@@ -136,7 +136,7 @@ public class Slave {
             BufferedImage screenshot = robot.createScreenCapture(screenSize);
             File outputfile = new File("slave_screenshot.png");
             ImageIO.write(screenshot, "png", outputfile);
-            String imagePath = "current/workin/path";
+            String imagePath = outputfile.getAbsolutePath();
             OutputStream oss = ss.getOutputStream();
             byte[] imageData = Files.readAllBytes(Paths.get(imagePath ));
             oss.write(imageData);
